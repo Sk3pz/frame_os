33b94c5a640010b1d03a31f3c19c49686a9c889b
Moocow9m <649310-Moocow9m@users.noreply.gitlab.com>
2020-09-03 22:51:42 -0700

    Prep bootloader_creator to run cargo and gcc commands.

--- bootloader_creator/src/main.rs
+++ bootloader_creator/src/main.rs
@@ -1,3 +1,115 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Command, Output};
+
+struct CommandLine {
+    base: String,
+    args: String,
+    windows: bool,
+}
+
+impl CommandLine {
+    fn run(&self) -> Output {
+        return if self.windows {
+            Command::new("cmd")
+                .args(&["/C", format!("{} {}", self.base, self.args).as_str()])
+                .output()
+                .expect("failed to execute process")
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg(format!("{} {}", self.base, self.args).as_str())
+                .output()
+                .expect("failed to execute process")
+        };
+    }
+}
+
+fn as_string(string: &mut Vec<String>) -> String {
+    if string.is_empty() {
+        return "".to_string();
+    }
+    let mut builder = "".to_string();
+    while !string.is_empty() {
+        let mut str = string.pop().unwrap();
+        while !str.is_empty() {
+            builder.push(str.pop().unwrap());
+        }
+        builder.push(' ');
+    }
+    builder.pop().unwrap();
+    string_reverse(builder)
+}
+
+fn as_string_primitive(string: &mut Vec<&str>) -> String {
+    let mut vec2 = vec![];
+    while !string.is_empty() {
+        vec2.push(string.pop().unwrap().to_string())
+    }
+    vec2.reverse();
+    as_string(&mut vec2)
+}
+
+fn string_reverse(mut string: String) -> String {
+    let mut builder = "".to_string();
+    while !string.is_empty() {
+        builder.push(string.pop().unwrap());
+    }
+    builder
+}
+
+/*fn craft_command(exe: String, mut args: Vec<String>) -> CommandLine {
+    CommandLine {
+        base: exe,
+        args: as_string(&mut args),
+    }
+}*/
+
+fn craft_command(exe: &str, mut args: Vec<&str>, is_windows: bool) -> CommandLine {
+    CommandLine {
+        base: exe.to_string(),
+        args: as_string_primitive(&mut args),
+        windows: is_windows,
+    }
+}
+
+fn craft_powershell_command(exe: &str, mut args: Vec<&str>) -> CommandLine {
+    let mut vec2 = vec!["-NoProfile", "-NonInteractive", "-command", exe];
+    while !args.is_empty() {
+        vec2.push(args.pop().unwrap());
+    }
+    return craft_command("powershell", vec2, true);
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{craft_command, craft_powershell_command};
+    #[test]
+    fn check_caller() {
+        let is_windows = cfg!(target_os = "windows");
+        let mut commands = vec![craft_command("echo", vec!["hello", "world"], is_windows)];
+        if is_windows {
+            commands.push(craft_powershell_command("pwd", vec![]));
+        } else {
+            commands.push(craft_command("pwd", vec![], false));
+        }
+        while !commands.is_empty() {
+            let command = commands.pop().unwrap();
+            let output = command.run();
+            if output.status.success() {
+                println!(
+                    "{}\nResult: {}",
+                    output.status,
+                    std::str::from_utf8(&output.stdout).unwrap()
+                );
+            } else {
+                eprintln!(
+                    "{}\nResult: {}",
+                    output.status,
+                    std::str::from_utf8(&output.stderr).unwrap()
+                )
+            }
+            assert!(output.status.success());
+        }
+    }
 }
