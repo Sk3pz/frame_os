4f7ef6a53a1a66f473003e9c8dd87665604e7c4a
DeathBySpork <ericshreve02@gmail.com>
2020-09-05 13:14:27 -0500

    implemented some basic file structues with no current uses and no functionality other than being a framework. (Had little time to do any work today)

--- simple_fs/src/lib.rs
+++ simple_fs/src/lib.rs
@@ -1,7 +1,71 @@
+
+pub struct Folder {
+    path: String
+}
+
+impl Folder {
+    pub fn new() -> Folder {
+
+    }
+    pub fn clone(&self) -> Folder {
+        Folder {
+            path: self.path.clone()
+        }
+    }
+    pub fn mkdir(&self) { // TODO: return error codes if fails or panic idk
+        // TODO: Implement
+    }
+    pub fn exists(&self) -> bool {
+        true // TODO: Implement
+    }
+}
+
+pub struct File {
+    path: Folder, // path without the file name
+    name: String, // name of the file
+    full_path: String // path + name
+}
+
+impl File {
+    pub fn new(name: &str, loc: Folder) -> File {
+
+        if !loc.exists() { // ensure the folder exists (if not we are creating)
+            loc.mkdir();
+        }
+
+        let full = &loc.path + &name;
+        let f = File {
+            path: loc,
+            name: name.to_string(),
+            full_path: full
+        };
+
+        if !f.exists() {
+            f.create();
+        }
+
+        f
+    }
+
+    pub fn create(&self) { // TODO: return error codes if fails or panic idk
+        // TODO: Implement
+    }
+
+    pub fn exists(&self) -> bool {
+        true // TODO: Implement
+    }
+
+}
+
+
+
 #[cfg(test)]
 mod tests {
+    use crate::Folder;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let root_folder: Folder = Folder::new();
+        root_folder.mkdir();
     }
 }
